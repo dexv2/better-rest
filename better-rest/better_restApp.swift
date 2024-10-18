932564b42aa8994062948583a350c5b8bf479b89
phil002109 <vermontphil.paguiligan@gmail.com>
2024-10-18 10:49:32 +0800

    Initial Commit

---
+++ better-rest/better_restApp.swift
@@ -0,0 +1,17 @@
+//
+//  better_restApp.swift
+//  better-rest
+//
+//  Created by Vermont Phil Paguiligan on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct better_restApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
